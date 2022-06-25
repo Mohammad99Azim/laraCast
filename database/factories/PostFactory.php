819da3ac1b24e53ab87d6ccf54c51dd8794825e6
Mohammad99Azim <azim99mohammad@gmail.com>
2022-06-25 17:14:03 +0300

    add more design for the posts handle grids and put the posts from DB

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
         return [
             'user_id' => User::factory(),
             "category_id" => Category::factory(),
-            "title" => $this->faker->title(),
+            "title" => $this->faker->text(100),
             "slug" => $this->faker->unique()->slug(),
             'excerpt' => $this->faker->sentence(),
             'body' => $this->faker->paragraph(10)
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,76 +1,21 @@
 @extends('./components/layout')
 @section('content')
     @include('_post-header')
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-main-post-card/>
+        @if ($allPosts)
+            <x-main-post-card :post="$allPosts[0]" />
+            @if ($allPosts->count() > 1)
+                <x-grid-posts-handle :posts="$allPosts->skip(1)"/>
+            @endif
+            <div class="lg:grid lg:grid-cols-3">
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+            </div>
+        @else
+            <p class="text-center">There is No Posts yet </p>
+        @endif
     </main>
 
 
 
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{{--
-
-@extends('./components/layout')
-@section('content')
-    <article>
-        @foreach ($allPosts as $post)
-            <article>
-                <a href="/post/{{ $post->slug }}">
-                    <h1> {{ $post->title }} </h1>
-                </a>
-                By <a href="/author/{{ $post->author->username }}"> {{ $post->author->name }} </a> in<a
-                    href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>
-                <p>{{ $post->excerpt }}</p>
-            </article>
-
-            @if ($loop->last)
-                <hr>
-            @endif
-        @endforeach
-
-        {{-- @foreach ($allPosts as $post)
-            <article>
-
-                {{ $post->matter('title') }}
-            </article>
-        @endforeach
-@endsection --}}
